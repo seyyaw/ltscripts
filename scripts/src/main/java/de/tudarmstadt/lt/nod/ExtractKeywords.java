35b59ede03b7b8a95e36630d9f30eed4d2a41e36
seyyaw <seidymam@gmail.com>
2016-03-25 16:10:39 +0100

    parallelizing

--- scripts/src/main/java/de/tudarmstadt/lt/nod/ExtractKeywords.java
+++ scripts/src/main/java/de/tudarmstadt/lt/nod/ExtractKeywords.java
@@ -9,9 +9,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Supplier;
 
 import org.apache.commons.io.IOUtils;
-
 import com.sree.textbytes.jtopia.Configuration;
 import com.sree.textbytes.jtopia.TermDocument;
 import com.sree.textbytes.jtopia.TermsExtractor;
@@ -20,34 +22,74 @@ public class ExtractKeywords {
 	private static Connection conn;
 	private static Statement st;
 
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException,
-			ClassNotFoundException, SQLException, IOException {
+	public static void main(String[] args)
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, IOException {
 		Configuration.setTaggerType("default");
 		Configuration.setSingleStrength(1);
 		Configuration.setNoLimitStrength(1);
 		Configuration.setModelFileLocation("model/default/english-lexicon.txt");
-		TermsExtractor termExtractor = new TermsExtractor();
-		TermDocument termDocument = new TermDocument();
-		initDB("cable","","","");
+		initDB("cable", "", "", "");
 		st.setFetchSize(50);
 		ResultSet docSt = st.executeQuery("select * from document;");
 
-		FileOutputStream os = new FileOutputStream("important-terms.tsv");
+		ThreadLocal<FileOutputStream> os = ThreadLocal.withInitial(new Supplier<FileOutputStream>() {
+			@Override
+			public FileOutputStream get() {
+				try {
+					return new FileOutputStream("important-terms-" + Thread.currentThread().getId() + ".tsv");
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+
+		});
+
+		ThreadLocal<TermsExtractor> termExtractor = ThreadLocal.withInitial(() -> new TermsExtractor());
+
+		ExecutorService t = Executors.newFixedThreadPool(50);
+
 		while (docSt.next()) {
+			count++;
 			String content = docSt.getString("content");
 			Long id = docSt.getLong("id");
-			termDocument = termExtractor.extractTerms(content);
-			Map<String, ArrayList<Integer>> extracted = termDocument.getFinalFilteredTerms();
-			for(String term:extracted.keySet()){				
-			IOUtils.write(id + "\t" + term +"\t" + extracted.get(term).get(0) + "\n", os, "UTF-8");
-			}
+
+			t.execute(new Runnable() {
+				@Override
+				public void run() {
+
+					TermDocument termDocument = termExtractor.get().extractTerms(content);
+					Map<String, ArrayList<Integer>> extracted = termDocument.getFinalFilteredTerms();
+
+					for (String term : extracted.keySet()) {
+						try {
+							IOUtils.write(id + "\t" + term + "\t" + extracted.get(term).get(0) + "\n", os.get(),
+									"UTF-8");
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					}
+
+				}
+			});
+
 		}
+
+		t.shutdown();
+		while (!t.isTerminated())
+			try {
+				Thread.sleep(10L);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
 	}
 
 	public static void initDB(String aDbName, String ip, String user, String pswd)
 			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
-		String url = "jdbc:postgresql://"+ip+"/";
+		String url = "jdbc:postgresql://" + ip + "/";
 		String dbName = aDbName;
 		String driver = "org.postgresql.Driver";
 		String userName = user;
